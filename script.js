534d733dbd88028961e88d915134ff2485190fbd
tihi <77891364+JustTizian@users.noreply.github.com>
2025-08-15 12:51:11 +0200

    Create game loop to play 5 rounds

--- script.js
+++ script.js
@@ -1,35 +1,44 @@
-let humanScore = 0;
-let computerScore = 0;
+playGame()
 
-function getComputerChoice(){
-    switch(Math.floor(Math.random() * 3) + 1){
+function getComputerChoice() {
+    switch (Math.floor(Math.random() * 3) + 1) {
         case 1: return "ROCK";
         case 2: return "PAPER";
         case 3: return "SCISSORS";
     }
 }
 
-function getHumanChoice(){
+function getHumanChoice() {
     return prompt("Your choice: ")
 }
 
-function playRound(humanChoice, computerChoice){
-    humanChoice = humanChoice.toUpperCase();
-    if(humanChoice === computerChoice){
-        console.log(`You tied! ${humanChoice} ties with ${computerChoice}`)
-    }else if(humanChoice === "ROCK" && computerChoice === "SCISSORS" 
-        || humanChoice === "SCISSORS" && computerChoice === "PAPER"
-        || humanChoice === "PAPER" && computerChoice === "ROCK"){
+
+
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+    let ties = 0;
+    
+    for (let index = 0; index < 5; index++) {
+        playRound(getHumanChoice(), getComputerChoice())
+    }
+    console.log("Human Score: " + humanScore)
+    console.log("Computer Score: " + computerScore)
+    console.log("Ties: " + ties)
+
+    function playRound(humanChoice, computerChoice) {
+        humanChoice = humanChoice.toUpperCase();
+        if (humanChoice === computerChoice) {
+            console.log(`You tied! ${humanChoice} ties with ${computerChoice}`)
+            ties++;
+        } else if (humanChoice === "ROCK" && computerChoice === "SCISSORS"
+            || humanChoice === "SCISSORS" && computerChoice === "PAPER"
+            || humanChoice === "PAPER" && computerChoice === "ROCK") {
             console.log(`You won! ${humanChoice} beats ${computerChoice}`)
             humanScore++;
-        }else{
+        } else {
             console.log(`You lost! ${computerChoice} beats ${humanChoice}`)
             computerScore++;
         }
-    
+    }
 }
-
-playRound("PAPER", "PAPER")
-playRound("SCISSORS", "PAPER")
-playRound("ROCK", "PAPER")
-playRound("ROCK", "SCISSORS")
