a82eeeebfbfc97f7846513aa114f42a38693443c
tihi <77891364+JustTizian@users.noreply.github.com>
2025-08-15 12:44:49 +0200

    Implemt basic game logic

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice(){
     switch(Math.floor(Math.random() * 3) + 1){
-        case 1: return "Rock";
-        case 2: return "Paper";
-        case 3: return "Scissors";
+        case 1: return "ROCK";
+        case 2: return "PAPER";
+        case 3: return "SCISSORS";
     }
 }
 
-function getUserChoice(){
+function getHumanChoice(){
     return prompt("Your choice: ")
 }
 
-console.log(getUserChoice())
-console.log(getUserChoice())
+function playRound(humanChoice, computerChoice){
+    humanChoice = humanChoice.toUpperCase();
+    if(humanChoice === computerChoice){
+        console.log(`You tied! ${humanChoice} ties with ${computerChoice}`)
+    }else if(humanChoice === "ROCK" && computerChoice === "SCISSORS" 
+        || humanChoice === "SCISSORS" && computerChoice === "PAPER"
+        || humanChoice === "PAPER" && computerChoice === "ROCK"){
+            console.log(`You won! ${humanChoice} beats ${computerChoice}`)
+            humanScore++;
+        }else{
+            console.log(`You lost! ${computerChoice} beats ${humanChoice}`)
+            computerScore++;
+        }
+    
+}
+
+playRound("PAPER", "PAPER")
+playRound("SCISSORS", "PAPER")
+playRound("ROCK", "PAPER")
+playRound("ROCK", "SCISSORS")
